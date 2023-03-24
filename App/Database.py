f4733cccfac7d6b49d732b807941573c219f619c
Sheyan Suraweeera <sheyan.suraweera@neosolax.com>
2023-03-25 01:25:06 +0530

    add databses

---
+++ App/Database.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+class Database:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+
+    def disconnect(self):
+        self.connection.close()
+        self.connection = None
