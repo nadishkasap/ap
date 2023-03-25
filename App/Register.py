0945232b96803a9a8d386b4d982787b1209e1a0f
Sheyan Suraweeera <sheyan.suraweera@neosolax.com>
2023-03-25 12:06:45 +0530

    database fix to register

--- App/Register.py
+++ App/Register.py
@@ -2,18 +2,19 @@ import tkinter as tk
 from tkinter import *
 from Database import Database
 
-class Register:
+
+class Register(object):
     def __init__(self):
-        self.database=Database
+        self.root = tk.Tk()
+        self.root.destroy()
+        self.database = Database
         global sup
         sup = Tk()
 
-        sup.title("Register - LMS University of Kelaniya")
-
-        fname = StringVar()
-        lname = StringVar()
-        passW = StringVar()
-        email = StringVar()
+        self.fname = StringVar()
+        self.lname = StringVar()
+        self.passW = StringVar()
+        self.email = StringVar()
 
         sup_canvas = Canvas(sup, width=720, height=440, bg="#600")
         sup_canvas.pack()
@@ -28,33 +29,33 @@ class Register:
         # firstname
         flabel = Label(sup_frame, text="First Name", fg='black', bg='white')
         flabel.place(relx=0.21, rely=0.4)
-        fname = Entry(sup_frame, bg='#d3d3d3', fg='black', textvariable=fname)
-        fname.config(width=42)
-        fname.place(relx=0.32, rely=0.4)
+        self.fname = Entry(sup_frame, bg='#d3d3d3', fg='black', textvariable=self.fname)
+        self.fname.config(width=42)
+        self.fname.place(relx=0.32, rely=0.4)
 
         # lastname
         ulabel = Label(sup_frame, text="Last Name", fg='black', bg='white')
         ulabel.place(relx=0.21, rely=0.5)
-        user = Entry(sup_frame, bg='#d3d3d3', fg='black', textvariable=lname)
-        user.config(width=42)
-        user.place(relx=0.32, rely=0.5)
+        self.lname = Entry(sup_frame, bg='#d3d3d3', fg='black', textvariable=self.lname)
+        self.lname.config(width=42)
+        self.lname.place(relx=0.32, rely=0.5)
 
         # email
         clabel = Label(sup_frame, text="Email", fg='black', bg='white')
         clabel.place(relx=0.215, rely=0.6)
-        c = Entry(sup_frame, bg='#d3d3d3', fg='black', textvariable=email)
-        c.config(width=42)
-        c.place(relx=0.32, rely=0.6)
+        self.email = Entry(sup_frame, bg='#d3d3d3', fg='black', textvariable=self.email)
+        self.email.config(width=42)
+        self.email.place(relx=0.32, rely=0.6)
 
         # password
         plabel = Label(sup_frame, text="Password", fg='black', bg='white')
         plabel.place(relx=0.215, rely=0.7)
-        pas = Entry(sup_frame, bg='#d3d3d3', fg='black', show="*", textvariable=passW)
-        pas.config(width=42)
-        pas.place(relx=0.32, rely=0.7)
+        self.passW = Entry(sup_frame, bg='#d3d3d3', fg='black', show="*", textvariable=self.passW)
+        self.passW.config(width=42)
+        self.passW.place(relx=0.32, rely=0.7)
 
         # signup BUTTON
-        sp = Button(sup_frame, text='SignUp', padx=5, pady=5, width=5,  bg='green',command=self.signUp)
+        sp = Button(sup_frame, text='SignUp', padx=5, pady=5, width=5, bg='green', command=self.signUp)
         sp.configure(width=15, height=1, activebackground="#33B5E5", relief=FLAT)
         sp.place(relx=0.4, rely=0.8)
 
@@ -67,10 +68,16 @@ class Register:
 
     def register(self):
         self.LoginPage()
+
     def signUp(self):
-        db=self.database('localhost','root','','quiz')
+        fname = self.fname.get()
+        lname = self.lname.get()
+        email = self.email.get()
+
+        db = self.database('localhost', 'root', '', 'quiz')
         db.connect()
         cursor = db.connection.cursor()
-        insertQuery = "INSERT INTO user (fname,lname,email,user_type) VALUES ({fname},{lname},{email},2);"
-        cursor.execute(insertQuery)
+        insertQuery = """INSERT INTO user (fname, lname, email,user_type) VALUES (%s, %s, %s,%s)"""
+        val = (fname, lname, email,2)
+        cursor.execute(insertQuery, val)
         db.connection.commit()
